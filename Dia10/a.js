3c0b28bd0eacad27435f242eb8306c5dcca74f72
Arthur Carvalho Ribeiro <arthurcribeiro5@gmail.com>
2023-12-19 11:27:46 -0300

    alterando o dia 10

--- Dia10/a.js
+++ Dia10/a.js
@@ -21,3 +21,29 @@ console.log('carros cadastrados')
 for(let i=0; i<modelos.length;i++){
     console.log(modelos[i]+'-'+anos[i]+ '-'+valores[i])
 }
+
+for(let i=0;i<valores.length -1; i++){
+    for(let j = 0; j<valores.length-i-1;j++){
+      if(valores[j] > valores[j+1]){
+
+
+    let modeloMaiorValor = modelos[j]
+    modelos[j] = modelos[j+1]
+    modelos[j+1] = modeloMaiorValor
+
+    let anoMaiorValor = anos[j]
+    anos[j]=anos[j+1]
+    anos[j+1]= anoMaiorValor
+
+    let maiorValor=valores[j]
+    valores[j]=valores[j+1]
+    valores[j+1]=maiorValor
+
+    }
+  }
+}
+
+console.log('carros ordenados pelo preço:')
+for (let i=0;i<modelos.length;i++){
+    console.log(modelos[i] + " - " + anos[i]+ ' - ' + valores[i])
+}
